b48a44b9fd8fa0abf4c1b0efee49c43edafeb81b
darrinlallen <darrin.lovard.allen@gmail.com>
2023-02-06 16:21:39 -0800

    everything

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -17,53 +17,104 @@ export default class AppClass extends React.Component {
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
 
-  getXY = () => {
-    // It it not necessary to have a state to track the coordinates.
-    // It's enough to know what index the "B" is at, to be able to calculate them.
-  }
-
-  getXYMessage = () => {
-    // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
-    // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
-    // returns the fully constructed string.
-  }
-
-  reset = () => {
-    // Use this helper to reset all states to their initial values.
-  }
-
-  getNextIndex = (direction) => {
-    // This helper takes a direction ("left", "up", etc) and calculates what the next index
-    // of the "B" would be. If the move is impossible because we are at the edge of the grid,
-    // this helper should return the current index unchanged.
-  }
-
-  move = (evt) => {
-    // This event handler can use the helper above to obtain a new index for the "B",
-    // and change any states accordingly.
-  }
-
-  onChange = (evt) => {
-    // You will need this to update the value of the input.
-  }
-
-  onSubmit = (evt) => {
-    // Use a POST request to send a payload to the server.
-  }
+  let initialMessage = ''
+  let initialEmail = ''
+  let initialSteps = 0
+  let initialIndex = 4 // the index the "B" is at
+  
+  export default function AppFunctional(props) {
+    // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
+    // You can delete them and build your own logic from scratch.
+    const  [moves, setmoves] = useState(0)
+    const [message, setmessage] = useState(initialMessage)
+    const [x, setx] = useState(2)
+    const [y, sety] = useState(2)
+    const [indy, setindy] = useState(initialIndex)
+    
+    function reset() {
+      // Use this helper to reset all states to their initial values.
+     initialMessage = ''
+    initialEmail = ''
+     setmoves(0)
+       setindy(4) // the index the "B" is at
+       setx(2)
+       sety(2)
+        }
+  
+    function onChangeL() {
+      // You will need this to update the value of the input    
+    if (indy >= 1){
+      setmessage('')
+      setindy(indy -1)
+    setx(x-1)
+    sety(y)
+    setmoves(moves+1)
+    }
+    else {
+      setmessage ("You can't go left")
+    setx(x)
+    sety(y)
+    }
+    }
+    function onChangeR() {
+      // You will need this to update the value of the input    
+    if (indy < 8){
+      setmessage('')
+      setindy(indy +1)
+    setx(x+1)
+    sety(y)
+    setmoves(moves+1)}
+    else {
+      setmessage("You can't go right")
+      setx(x)
+      sety(y)
+    }
+    }
+    function onChangeU() {
+      // You will need this to update the value of the input    
+   if (y > 1){
+    setmessage('')
+      setindy(indy -3)
+    sety(y-1)
+    setx(x)
+    setmoves(moves+1)}
+    else {
+      setmessage("You can't go up")
+      setx(x)
+      sety(y)
+      setindy(indy)
+    }
+    }
+    function onChangeD() {
+      // You will need this to update the value of the input    
+    if (y < 3){
+      setmessage('')
+      setindy(indy + 3)
+    sety(y+1)
+    setx(x)
+    setmoves(moves+1)}
+    else {
+      setmessage("You can't go down")
+      sety(y)
+      setx(x)
+    }
+    }
 
   render() {
     const { className } = this.props
+
     return (
       <div id="wrapper" className={className}>
+        {console.log(className)}
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">Coordinates ({x}, {y})</h3>
+          <h3 id="steps">You moved {moves} times</h3>
         </div>
         <div id="grid">
           {
             [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-              <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-                {idx === 4 ? 'B' : null}
+              <div key={idx} className={`square${idx === indy ? ' active' : ''}`}>
+                {idx === indy ? 'B' : null}
               </div>
             ))
           }
@@ -72,14 +123,14 @@ export default class AppClass extends React.Component {
           <h3 id="message"></h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button onClick={onChangeL} id="left">LEFT</button>
+          <button onClick={onChangeU} id="up">UP</button>
+          <button onClick={onChangeR} id="right">RIGHT</button>
+          <button onClick= {onChangeD} id="down">DOWN</button>
+          <button onClick ={reset} id="reset">reset</button>
         </div>
         <form>
-          <input id="email" type="email" placeholder="type email"></input>
+          <input id="email" type="email" placeholder="type email" required ></input>
           <input id="submit" type="submit"></input>
         </form>
       </div>
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import { useState } from "react"
+import React  from 'react'
 
 // Suggested initial states
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 // the index the "B" is at
+let initialMessage = ''
+let initialEmail = ''
+let initialSteps = 0
+let initialIndex = 4 // the index the "B" is at
 
 export default function AppFunctional(props) {
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
-
+  const  [moves, setmoves] = useState(0)
+  const [message, setmessage] = useState(initialMessage)
+  const [x, setx] = useState(2)
+  const [y, sety] = useState(2)
+  const [indy, setindy] = useState(initialIndex)
+  
   function getXY() {
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
@@ -23,21 +29,83 @@ export default function AppFunctional(props) {
 
   function reset() {
     // Use this helper to reset all states to their initial values.
+   initialMessage = ''
+  initialEmail = ''
+   setmoves(0)
+     setindy(4) // the index the "B" is at
+     setx(2)
+     sety(2)
+    
   }
 
   function getNextIndex(direction) {
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
-  }
+  }   
 
   function move(evt) {
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
   }
 
-  function onChange(evt) {
-    // You will need this to update the value of the input.
+  function onChangeL() {
+    // You will need this to update the value of the input    
+  if (indy >= 1){
+    setmessage('')
+    setindy(indy -1)
+  setx(x-1)
+  sety(y)
+  setmoves(moves+1)
+  }
+  else {
+    setmessage ("You can't go left")
+  setx(x)
+  sety(y)
+  }
+  }
+  function onChangeR() {
+    // You will need this to update the value of the input    
+  if (indy < 8){
+    setmessage('')
+    setindy(indy +1)
+  setx(x+1)
+  sety(y)
+  setmoves(moves+1)}
+  else {
+    setmessage("You can't go right")
+    setx(x)
+    sety(y)
+  }
+  }
+  function onChangeU() {
+    // You will need this to update the value of the input    
+ if (y > 1){
+  setmessage('')
+    setindy(indy -3)
+  sety(y-1)
+  setx(x)
+  setmoves(moves+1)}
+  else {
+    setmessage("You can't go up")
+    setx(x)
+    sety(y)
+    setindy(indy)
+  }
+  }
+  function onChangeD() {
+    // You will need this to update the value of the input    
+  if (y < 3){
+    setmessage('')
+    setindy(indy + 3)
+  sety(y+1)
+  setx(x)
+  setmoves(moves+1)}
+  else {
+    setmessage("You can't go down")
+    sety(y)
+    setx(x)
+  }
   }
 
   function onSubmit(evt) {
@@ -47,14 +115,15 @@ export default function AppFunctional(props) {
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="coordinates">Coordinates ({x}, {y})</h3>
+        <h3 id="steps">You moved {moves} times</h3>
+        <h3 id="message">{message} </h3>
       </div>
       <div id="grid">
         {
           [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
+            <div key={idx} className={`square${idx === indy ? ' active' : ''}`}>
+              {idx === indy ? 'B' : null}
             </div>
           ))
         }
@@ -63,11 +132,11 @@ export default function AppFunctional(props) {
         <h3 id="message"></h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button onClick={onChangeL} id="left">LEFT</button>
+        <button  onClick={onChangeU} id="up">UP</button>
+        <button onClick={onChangeR} id="right">RIGHT</button>
+        <button onClick={onChangeD} id="down">DOWN</button>
+        <button onClick={reset} id="reset">reset</button>
       </div>
       <form>
         <input id="email" type="email" placeholder="type email"></input>
@@ -76,3 +145,4 @@ export default function AppFunctional(props) {
     </div>
   )
 }
+
